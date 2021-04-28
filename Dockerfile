a0863be4732bdbcff6439ca7d77f300e84d67536
M S <mixa.ru.ru@gmail.com>
2021-04-28 11:00:59 +0300

    refactord + ENV variable in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ COPY cmd cmd
 RUN CGO_ENABLED=0 GOOS=linux go build -a -o /app cmd/main.go
 
 FROM alpine:3.9
+ENV CS_API=https://api
 EXPOSE 8811
 COPY --from=build /app /bin
 CMD ["/bin/app"]
--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/patrickmn/go-cache"
 )
 
-var api = "https://api"
-var hotCache = cache.New(30*time.Second, 10*time.Minute)
-var coldCache = cache.New(-1, -1)
+var api = os.Getenv("CS_API")
+var hotCache = cache.New(45*time.Second, 5*time.Minute)
+var coldCache = cache.New(-1, -1) // cache that does not expire , only update
 
 func loadData(w http.ResponseWriter, req *http.Request) {
 
@@ -21,24 +22,28 @@ func loadData(w http.ResponseWriter, req *http.Request) {
 	cachedResponse, found := hotCache.Get(url)
 
 	if found {
-		fmt.Println("Cached: " + url)
-		coldCache.SetDefault(url, cachedResponse)
-		fmt.Fprintf(w, cachedResponse.(string))
+		fmt.Printf("Page from HOT cache: %s\n", url)
+		coldCache.SetDefault(url, cachedResponse.(string))
+		fmt.Fprint(w, cachedResponse.(string))
+		return
 	} else {
 
 		client := http.Client{
-			Timeout: 5 * time.Second,
+			Timeout: 60 * time.Second,
 		}
 
 		resp, err := client.Get(url)
 
 		if err != nil {
+
 			cachedResponse, found = coldCache.Get(url)
 
 			if found {
-				fmt.Fprintf(w, cachedResponse.(string))
+				fmt.Printf("Page from COLD cache: %s\n", url)
+				fmt.Fprint(w, cachedResponse.(string))
 			} else {
-				fmt.Fprintf(w, "") // todo : send response with error code
+				fmt.Println("Worst case, nothing sent to browser")
+				fmt.Fprint(w, "") // todo : send response with error code
 			}
 
 			return
@@ -52,17 +57,15 @@ func loadData(w http.ResponseWriter, req *http.Request) {
 			log.Fatal(err)
 		}
 
-		fmt.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
-
 		bodyString := string(bodyBytes)
 
 		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 			hotCache.Set(url, bodyString, cache.DefaultExpiration)
 			coldCache.SetDefault(url, bodyString)
-			fmt.Println("HTTP Status is in the 2xx range")
+			fmt.Printf("Requested page, set coldCache and hotCache %s\n", url)
 		} else {
-			cachedResponse, found = coldCache.Get(url) // handle case when cold cache not found
-			fmt.Println("Error HTTP Status code")
+			fmt.Printf("Page from COLD cache #2: %s\n", url)
+			cachedResponse, found = coldCache.Get(url) // handle case when cache not found
 			bodyString = cachedResponse.(string)
 		}
 
